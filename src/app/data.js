e753d428e9da62e96c3738ce896308bd43664799
Nicopipio <nicoar232346@gmail.com>
2024-02-22 20:08:10 -0600

    Guia4

---
+++ src/app/data.js
@@ -0,0 +1,72 @@
+export const data=[
+    {
+        id:1,
+        title:'Cien años de soledad',
+        price:100,
+        urlImage:'https://images.penguinrandomhouse.com/cover/9780525562443',
+        quantity:1
+    },
+    {
+        id:2,
+        title:'El señor de los anillos(Trilogía)',
+        price:190,
+        urlImage:'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+        quantity:1
+    },
+    {
+        id:3,
+        title:'Cuentos de Barro',
+        price:30,
+        urlImage:'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+        quantity:1
+    },
+    {
+        id:4,
+        title:'Tierra de Infancia',
+        price:30,
+        urlImage:'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+        quantity:1
+    }, 
+    {
+        id:5,
+        title:'Harry Potter Pack',
+        price:390,
+        urlImage:'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+        quantity:1
+    },
+    {
+        id:6,
+        title:'Bodes de sangre',
+        price:190,
+        urlImage:'https://images.cdn1.buscalibre.com/fit-in/360x360/1b/7a/1b7a2c9cf9f43d1eade00bc6fd7256ad.jpg',
+        quantity:1
+    },
+    {
+        id:7,
+        title:'IT',
+        price:400,
+        urlImage:'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/It_%281986%29_front_cover%2C_first_edition.jpg/800px-It_%281986%29_front_cover%2C_first_edition.jpg',
+        quantity:1
+    },
+    {
+        id:8,
+        title:'22/11/63',
+        price:380,
+        urlImage:'https://m.media-amazon.com/images/I/913fIyrmceL._AC_UF894,1000_QL80_.jpg',
+        quantity:1
+    },
+    {
+        id:9,
+        title:'El resplandor',
+        price:370,
+        urlImage:'https://quelibroleo.com/images/libros/libro-1664955434.jpg',
+        quantity:1
+    },
+    {
+        id:10,
+        title:'Carrie',
+        price:390,
+        urlImage:'https://images.cdn1.buscalibre.com/fit-in/360x360/25/3b/253b35902bbebc059355fa26c19d9dc4.jpg',
+        quantity:1
+    }
+];
---
+++ src/components/Headers.jsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { useState } from 'react';
+
+export const Headers = ({
+  allProducts,
+  setAllProducts,
+  total,
+  countProducts,
+  setCountProducts,
+  setTotal,
+}) => {
+  const [active, setActive] = useState(false);
+
+  const onDeleteProduct = (product) => {
+    const confirmarEliminar = window.confirm(`¿Seguro que deseas eliminar ${product.title} del carrito?`);
+
+    if (confirmarEliminar) {
+      const resultados = allProducts.filter((item) => item.id !== product.id);
+      setTotal(total - product.price * product.quantity);
+      setCountProducts(countProducts - product.quantity);
+      setAllProducts(resultados);
+    }
+  };
+
+  const onCleanCart = () => {
+    const confirmarVaciar = window.confirm('¿Seguro que deseas vaciar el carrito de compras?');
+
+    if (confirmarVaciar) {
+      setAllProducts([]);
+      setTotal(0);
+      setCountProducts(0);
+    }
+  };
+  
+
+  return (
+    <header>
+      <h1>Tienda de Libros</h1>
+      <div className='container-icon'>
+        <div className='container-cart-icon' onClick={() => setActive(!active)}>
+          <img src="https://e7.pngegg.com/pngimages/833/426/png-clipart-black-shopping-cart-icon-for-free-black-shopping-cart.png" alt="carrito" className="icon-cart" />
+          <div className='count-products'>
+            <span id='contador-productos'>{countProducts}</span>
+          </div>
+        </div>
+        <div className={`container-cart-products ${active ? '' : 'hidden-cart'}`}>
+          {allProducts.length ? (
+            <>
+              <div className='row-product'>
+                {allProducts.map(product => (
+                  <div className='cart-product' key={product.id}>
+                    <div className='info-cart-product'>
+                      <img src={product.urlImage} alt={product.title} className='product-image' />
+                      <span className='cantidad-producto-carrito'>
+                        {product.quantity}
+                      </span>
+                      <p className='titulo-producto-carrito'>
+                        {product.title}
+                      </p>
+                      <span className='precio-producto-carrito'>
+                        ${product.price}
+                      </span>
+                    </div>
+                    <img src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png" alt="cerrar" className="icon-close" onClick={() => onDeleteProduct(product)} />
+                  </div>
+                ))}
+              </div>
+              <div className='cart-total'>
+                <h3>Total:</h3>
+                <span className='total-pagar'>${total}</span>
+              </div>
+              <button className='btn-clear-all' onClick={onCleanCart}>
+                Vaciar Carrito
+              </button>
+            </>
+          ) : (
+            <p className='cart-empty'>El carrito está vacío</p>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
